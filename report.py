650dd03cad0834fccd04f144b9a1959601ea7144
Kyi <klaye@uci.edu>
2026-02-06 06:34:14 +0000

    implemented uniquePage func

--- report.py
+++ report.py
@@ -1,7 +1,23 @@
-import stats.txt
+"""
+This is for answering the questions of the assignment. 
+"""
 
 def uniquePage():
-    pass
+    """
+    Description:
+    Since our stats.txt file contains valid unique url pages that are allowed to crawl, we need to 
+    open our stats.txt file and count the links
+
+    param:
+
+    return(int): 
+    count of lines in stats.txt
+    """
+    count = 0
+    with open('stats.txt', 'r') as f:
+        for url in f:
+            count += 1
+    return count
 
 def getLongestPage():
     pass
@@ -11,3 +27,10 @@ def getCommonWords():
 
 def getSubdomainList():
     pass
+
+def main():
+    num_of_unique_page = uniquePage()
+    print(f"Num of unique pages: {num_of_unique_page}")
+
+if __name__ == "__main__":
+    main()
