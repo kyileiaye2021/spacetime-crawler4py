5a7ac9d00d14f72bdc738d4d300e482b39c285cf
Kyi <klaye@uci.edu>
2026-02-06 06:58:03 +0000

    Updated uniquePage_longestPage to answer first 2 questions

--- report.py
+++ report.py
@@ -1,26 +1,33 @@
 """
-This is for answering the questions of the assignment. 
+This file is for answering the questions of the assignment. 
 """
 
-def uniquePage():
+def uniquePage_longestPage():
     """
     Description:
-    Since our stats.txt file contains valid unique url pages that are allowed to crawl, we need to 
-    open our stats.txt file and count the links
+    Since our stats.txt file contains valid unique url pages that are allowed to crawl,
+    we open our stats.txt file and count the links. At the same time, we keep track of the longest page.
 
-    param:
+    param: None
 
     return(int): 
     count of lines in stats.txt
     """
-    count = 0
-    with open('stats.txt', 'r') as f:
-        for url in f:
-            count += 1
-    return count
+    num_of_unique_page = 0
+    longest_page = ''
+    longest_page_word_count = 0
 
-def getLongestPage():
-    pass
+    with open('stats.txt', 'r') as f:
+
+        for line in f:
+            parts = line.strip().split("\t")
+
+            if int(parts[1]) > longest_page_word_count:
+                longest_page = parts[0]
+                longest_page_word_count = int(parts[1])
+
+            num_of_unique_page += 1
+    return num_of_unique_page, longest_page
 
 def getCommonWords():
     pass
@@ -29,8 +36,9 @@ def getSubdomainList():
     pass
 
 def main():
-    num_of_unique_page = uniquePage()
+    num_of_unique_page, longest_page = uniquePage_longestPage()
     print(f"Num of unique pages: {num_of_unique_page}")
+    print(f"Longest page: {longest_page}")
 
 if __name__ == "__main__":
     main()
